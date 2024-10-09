479c5a1f75da65dedf56fc9710f686d26f4ac45a
Leone <leon.fajardoperalta@elev.ga.ntig.se>
2024-10-09 15:08:50 +0200

    liug,juyg

---
+++ microNewStore/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int Namn = 1;
+bool ohemgee = false;
+
+while (ohemgee == false )
+{
+    string Ladd = Console.ReadLine();
+
+    ohemgee = int.TryParse(Ladd, out Namn);
+
+    if (ohemgee == false)
+    {
+        Console.WriteLine("Skriv en siffra, dumhuvve");
+    }
+}
+
+
+
+for (int i = 0; i < Namn; i++)
+{
+    if (i >= 5)
+    {
+        Console.WriteLine("frog" + i);
+    }
+    else
+    {
+        Console.WriteLine("dog" + i);
+
+    }
+}
+
+
+
+Namn = 1;
+ohemgee = false;
+
+while (ohemgee == false )
+{
+    string Ladd = Console.ReadLine();
+
+    ohemgee = int.TryParse(Ladd, out Namn);
+
+    if (ohemgee == false)
+    {
+        Console.WriteLine("Skriv en siffra, dumhuvve");
+    }
+}
+
+Console.WriteLine(Namn * 2 + Namn/2 + Namn + 2 + Namn - 2);
+
+
+Console.ReadLine();
